03aa0381f7fba736e612739d2204f1dadb29e388
Ki Seki <Ki_Seki@outlook.com>
2021-02-15 08:32:27 +0800

    comprehensively correct displaying problem

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ document.getElementById("number").innerHTML = days;
 document.getElementById("suffix").innerHTML = getSuffix(days);
 
 // 运行天数显示样式
-var numberWidth = String(days).length * 27;
-var counterWidth = 350;  // <body> 默认宽度
-var suffixWidth = (counterWidth - numberWidth) / 2;
-document.getElementById("number").style.width = numberWidth + "px";
-document.getElementById("preffix").style.width = suffixWidth + "px";
-document.getElementById("suffix").style.width = suffixWidth + "px";  // 平衡视觉效果
+var len = String(days).length;
+if (len > 2)
+{
+    var bias = (len - 2) * 15;  // 计算偏置量，平衡视觉效果
+    document.getElementById("number").style.width = 185 + bias + "px";
+    document.getElementById("suffix").style.width = 165 - bias + "px";
+}
 
 function getRunningDays() {
     var start = new Date(2021, 2 - 1, 3).getTime();
@@ -18,6 +19,7 @@ function getRunningDays() {
 }
 
 function getSuffix(days) {
+    if (11 <= days % 100 && days % 100 < 20) return "th.";
     var suffixs = {0: "th.", 1: "st.", 2: "nd.", 3: "rd."};
     if (days % 10 < 4) return suffixs[runningDays % 10];
     else return suffixs[0];
